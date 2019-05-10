b446b14a7136b2b81deb288920ca6bd6ef1bb707
Issa Janfaoui <you@example.com>
2019-05-10 18:02:44 +0200

    init ws authentification

---
+++ portail_promotion_ws_authentification/src/main/java/fr/afcepf/al33/wsauthentification/util/FollowableElementType.java
@@ -0,0 +1,10 @@
+package fr.afcepf.al33.wsauthentification.util;
+
+public interface FollowableElementType {
+	
+	String CATEGORY = "CATEGORY";
+	String SHOP = "SHOP";
+	String REFERENCE = "REFERENCE";
+	String PRODUCT = "PRODUCT";
+
+}
---
+++ portail_promotion_ws_authentification/src/main/java/fr/afcepf/al33/wsauthentification/util/SubscriberType.java
@@ -0,0 +1,5 @@
+package fr.afcepf.al33.wsauthentification.util;
+
+public interface SubscriberType {
+	String CLIENT = "CLIENT";
+}
